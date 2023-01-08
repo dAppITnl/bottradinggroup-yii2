7cb70b7bf7259ee4177c3e0f179aa8a43b463b42
dAppITnl <github@dappit.nl>
2023-01-08 13:45:29 +0100

    add params.example.php files

---
+++ console/config/params.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'webadmin@bottradinggroup.nl',
+];
+
+
+
---
+++ hftadmin/config/params.example.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'adminEmail' => 'webadmin@bottradinggroup.nl',
+    'bsVersion' => '4.x',
+
+//------------------------//
+// SYSTEM SETTINGS
+// get: Yii::$app->params[''];
+//------------------------//
+
+  /*
+  * webroot of iamges
+  */
+  'imagesmap' => 'https://hftadmin.bottradinggroup.nl/images/',
+
+  'companyNameFor2FA' => 'Bot Trading Group',
+
+  'discord_webhook'     => 'https://discord.com/api/v6',
+  'discord_bot_token'   => 'OTA5MTkwMjkxMzkzNzQ5MDYy.YZArcQ.770Qd6sdRpLLTOaMz1crZO291QE',
+  'discord_bot_token_2' => 'OTA5MzU5ODQ5MzE2NDIxNjYy.YZDJWw.4UA4UoWhPZD2M4mJFa97sBK2lcw',
+  'discord_server_id'   => '885495602144772117',
+
+  'moralis_api_url' => 'https://deep-index.moralis.io/api/v2',
+  'moralis_api_key' => 'Fj3MqavwcgAMxxpIGKhw7MXPSkZFQaGMOHMv1fgJNAoVQ6RijOs40QZZfomiEbss',
+
+
+];
